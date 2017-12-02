a6ab4e5161b5c64b358f8109b8dced5a9256c0a3
jacrproyect <jcastillo@jacrproyectosweb.com>
2017-12-02 17:44:20 -0500

    data-actualizada
    Se actualizo la data con servidor en la nube

--- proyectot3ajax/src/java/modelo/data.java
+++ proyectot3ajax/src/java/modelo/data.java
@@ -5,13 +5,32 @@
  */
 package modelo;
 
+import com.mysql.jdbc.Driver;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author HP
  */
 public class data {
     
-    
+        public Connection getMysql(){
+        String usuario="jacrproy_castill";
+        String clave = "jacrproyectosweb191295";
+        String url = "jdbc:mysql://jacrproyectosweb.com:3306/jacrproy_proyectot3-sowad";
+        Connection con = null;
+        try {
+            DriverManager.registerDriver(new Driver());
+            con = DriverManager.getConnection(url, usuario, clave);
+        } catch (SQLException ex) {
+            Logger.getLogger(data.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return con;
+    }
     
     
 }
